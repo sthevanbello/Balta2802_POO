ba153ab623590034e77aeaf4a0077b0dcbdb4647
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-11-18 20:23:14 -0300

    Encapsulamento - Conceito simples

---
+++ Payments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Payments");
+            // Objeto é um tipo de referência.
+            // Armazena o endereço apenas
+            var customer = new Customer();
+            customer.Name = "Sthevan";
+
+            Console.WriteLine(customer.Name);
+
+        }
+
+        public class Customer
+        {
+            public string Name { get; set; }
+        }
+
+        public class Payment
+        {
+            //Properties
+            public DateTime DueDate { get; set; }
+
+            //Methods
+            void Pagar(){}
+        }
+    }
+}
