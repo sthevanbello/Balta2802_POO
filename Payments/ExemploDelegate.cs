248273344dc0c6e2b4d887fb8ef0bcb46717dac6
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-11-23 16:29:08 -0300

    Delegate

---
+++ Payments/ExemploDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payments
+{
+    public class ExemploDelegate
+    {
+        public static void RealizarPagamento(double valor)
+        {
+            Console.WriteLine($"Pago o valor de {valor}");
+        }
+    }
+}
--- Payments/Program.cs
+++ Payments/Program.cs
@@ -7,80 +7,12 @@ namespace Payments
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Payments");
-            // Objeto é um tipo de referência.
-            // Armazena o endereço apenas
-            var customer = new Customer();
-            customer.Name = "Sthevan";
 
-            Console.WriteLine(customer.Name);
+            var pagar = new Pagamento.Pagar(ExemploDelegate.RealizarPagamento);
+            pagar(valor:1500);
 
-            var payment1 = new TicketPayment();
-            var payment2 = new CardPayment();
-
-            payment1.DueDate = DateTime.Now;
-
-            //var payment = new Payment();
-
-            Console.WriteLine(payment1);
-
+            Console.ReadKey();
         }
-        // sealed deixa a classe proibida de ser instanciada
-        //public sealed class Payment
-
-        // Classe abstrata para evitar instâncias
-        public abstract class Payment
-        //public class Payment
-        {
-            //Properties
-            public DateTime DueDate { get; set; }
-
-            public Payment()
-            {
-                Console.WriteLine("Construtor da classe base Payment");
-            }
-
-
-            //Methods
-            public virtual void Pagar() { }
-        }
-
-        public class Customer
-        {
-            public string Name { get; set; }
-        }
-
-        public class TicketPayment : Payment, IPayment
-        {
-            public override void Pagar()
-            {
-                // Regra do boleto com polimorfismo (Override)
-            }
-
-            public override string ToString()
-            {
-                return $"Class Ticket payment - Date: {DueDate}";
-            }
-
-            //Implemented by interface
-            public void PaymentMethod()
-            {
-                Console.WriteLine("Payment method implemented by interface");
-            }
-        }
-
-        public class CardPayment : Payment
-        {
-            public string Number { get; set; }
-
-
-        }
-
-        // Example Interface
-        public interface IPayment
-        {
-            void PaymentMethod();
-
-        }
+       
     }
 }
